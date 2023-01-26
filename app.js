ad844c01cbb4086e2f6fc3012ab6f6470f86227b
Cristian Bejenariu <bejenariu.cristian@digitalexplorer.ro>
2023-01-26 17:01:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+let noOfItems = 0;
+
+// add item handler
+function addItem() {
+  const message = document.querySelector(".message");
+  const inputValue = document.getElementById("Input").value;
+  const itemName = document.createTextNode(inputValue);
+  const empty = document.querySelector(".empty");
+  const li = document.createElement("li");
+  const close = document.getElementsByClassName("close");
+  const el = document.createElement("SPAN");
+  const x = document.createTextNode("\u00D7");
+
+  li.className = "list-group-item item";
+  li.appendChild(itemName);
+  if (inputValue === "") {
+    message.textContent = "You must type a name for the item!";
+  } else {
+    message.textContent = "";
+    empty.textContent = "";
+    noOfItems++;
+    document.getElementById("List").appendChild(li);
+  }
+  document.getElementById("Input").value = "";
+
+  // add close button
+  el.className = "close";
+  el.appendChild(x);
+  li.appendChild(el);
+
+  // close button handler
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      const div = this.parentElement;
+      div.classList.add("removed");
+      noOfItems--;
+      // check if list has items
+      if (noOfItems === 0) {
+        empty.textContent = "Your list is empty.";
+      }
+    };
+  }
+}
+
+// checked item handler
+const mainList = document.getElementById("List");
+mainList.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  }
+});
